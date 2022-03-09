e8bc3626ae139835ba294af9908c62a414f7b0b9
seba2santos <seba2santos@live.com>
2022-03-09 09:53:56 -0300

    proyecto react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
-    <div className="App">
+    <><NavBar /><div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div></>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+function NavBar() {
+  return (
+   <nav className='nav-bar'>
+        <ul>
+        <li>Productos</li>
+        <li>Diseños</li>
+        <li>Acerca de</li>
+        <li>Promociones</li>
+        
+    </ul>
+    <>
+        <button className='btn'>Shop</button>
+    </>
+   </nav>
+  )
+}
+
+export default NavBar
